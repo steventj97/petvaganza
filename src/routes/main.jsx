c09e497c49081fac45d35cb7c2c2420c93b44a65
steven <tjang@stevens-MacBook-Pro.local>
2019-10-07 00:04:53 +0700

    finalize sidebar

--- src/routes/main.jsx
+++ src/routes/main.jsx
@@ -1,19 +1,88 @@
 import React, { Component } from 'react'
-import { Button } from 'antd';
+import { Menu, Icon , Layout } from 'antd';
+import './main.css'
+
 import {Link} from 'react-router-dom'
 
+const { SubMenu } = Menu;
+const { Header, Footer, Sider, Content } = Layout;
+
 class main extends Component {
+    state = {
+        collapsed: false,
+      };
+    
+      toggle = () => {
+        this.setState({
+          collapsed: !this.state.collapsed,
+        });
+      };
     render() {
         return (
-            <div>
-                <h1>this is main page</h1>
-                <Link to="/home">
-                    <Button>Show the home</Button>
-                </Link>
-                <Link to="/intro">
-                    <Button>Show the intro</Button>
-                </Link>
-            </div>
+            <Layout>
+            <Sider width= {270} style={{height:"100vh"}} trigger={null} collapsible collapsed={this.state.collapsed}>
+              <div className="logo" />
+              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+            </Sider>
+            <Layout>
+              <Header style={{ background: '#fff', padding: 0 }}>
+                <Icon
+                  className="trigger"
+                  style={{marginLeft:"30px",fontSize:"18px"}}
+                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                  onClick={this.toggle}
+                />
+              </Header>
+              <Content
+                style={{
+                  margin: '24px 16px',
+                  padding: 24,
+                  background: '#fff',
+                  minHeight: 280,
+                }}
+              >
+                Content
+              </Content>
+            </Layout>
+          </Layout>
         )
     }
 }
